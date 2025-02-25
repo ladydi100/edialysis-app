15b54da85ba2bd9be8014fa876322973be83ec7c
Roadgar <roadgar2@gmail.com>
2025-02-25 22:12:10 +0100

    Iconos-nav-bar

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,17 +1,40 @@
-
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import HomePage from '../pages/HomePage';
-import MedicationPage from '../pages/MedicationPage'; 
-import HealthPage from '../pages/HealthPage'; 
-import NutritionPage from '../pages/NutritionPage'; 
-import AnalyticsPage from '../pages/AnalyticsPage'; 
+import MedicationPage from '../pages/MedicationPage';
+import HealthPage from '../pages/HealthPage';
+import NutritionPage from '../pages/NutritionPage';
+import AnalyticsPage from '../pages/AnalyticsPage';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Medicación') {
+            iconName = focused ? 'medkit' : 'medkit-outline';
+          } else if (route.name === 'Salud') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          } else if (route.name === 'Nutrición') {
+            iconName = focused ? 'fast-food' : 'fast-food-outline';
+          } else if (route.name === 'Analítica') {
+            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { paddingBottom: 5, height: 60 },
+      })}
+    >
       <Tab.Screen name="Home" component={HomePage} />
       <Tab.Screen name="Medicación" component={MedicationPage} />
       <Tab.Screen name="Salud" component={HealthPage} />
--- services/authService.js
+++ services/authService.js
@@ -6,7 +6,18 @@ export const loginUser = async (email, password) => {
     const response = await axios.post(`${API_URL}/login`, { email, password });
     return response.data; // Devuelve los datos 
   } catch (error) {
-   // console.error('Error en loginUser:', error);
+    console.error('Error en loginUser:', {
+      message: error.message,
+      code: error.code,
+      config: error.config,
+      request: error.request ? "Sí" : "No",
+      response: error.response ? {
+        status: error.response.status,
+        data: error.response.data,
+        headers: error.response.headers
+      } : "No hay respuesta del servidor"
+    });
+
     throw new Error(error.response?.data?.error || 'Error al iniciar sesión');
   }
 };
